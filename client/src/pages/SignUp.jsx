badea8ea9e7e79f99b6144d4907d275e25a44c54
Sivavarshini <sivu1103@gmail.com>
2024-12-11 08:13:05 +0530

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 //import OAuth from '../components/OAuth';
 
 export default function SignUp() {
-  /*const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -30,42 +30,42 @@ export default function SignUp() {
         setError(true);
         return;
       }
-      navigate('/sign-in');
+      //navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
     }
-  };*/
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form /*onSubmit={handleSubmit} */className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Username'
           id='username'
           className='bg-slate-100 p-3 rounded-lg'
-          //onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='Email'
           id='email'
           className='bg-slate-100 p-3 rounded-lg'
-          //onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='Password'
           id='password'
           className='bg-slate-100 p-3 rounded-lg'
-          //onChange={handleChange}
+          onChange={handleChange}
         />
         <button
-          disabled
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-         Sign Up
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
         
       </form>
@@ -75,7 +75,7 @@ export default function SignUp() {
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
-      
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
   );
 }
