f194808ae9a1aa0e8909b193f10b8ca66099505e
Sivavarshini <sivu1103@gmail.com>
2024-12-11 07:11:08 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,8 +13,7 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    res.status(500).json(error.message);
-    //next(error);
+    next(error);
   }
 };
 
--- api/index.js
+++ api/index.js
@@ -39,8 +39,8 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-
-/*app.use((err, req, res, next) => {
+//middleware
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
@@ -49,4 +49,3 @@ app.use('/api/auth', authRoutes);
     statusCode,
   });
 });
-*/
