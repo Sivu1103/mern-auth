82393a586612d93b2f8880eada609cf39406d6d9
Sivavarshini <sivu1103@gmail.com>
2024-12-11 06:51:53 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,18 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-//import authRoutes from './routes/auth.route.js';
+import authRoutes from './routes/auth.route.js';
 //import cookieParser from 'cookie-parser';
 //import path from 'path';
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
+
+
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.log(err);
+
+
   });
 
 //const __dirname = path.resolve();
@@ -26,7 +29,7 @@ const app = express();
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 */
-//app.use(express.json());
+app.use(express.json());
 
 //app.use(cookieParser());
 
@@ -35,7 +38,7 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
-//app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 /*app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
