ac0738bea977c8f2283c170c46cad40c3587eca4
Sivavarshini <sivu1103@gmail.com>
2024-12-13 10:02:34 +0530

    navigate completed

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -5,11 +5,12 @@ import { GoogleAuthProvider,
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function OAuth() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
         try {
           const provider = new GoogleAuthProvider();
@@ -31,7 +32,7 @@ export default function OAuth() {
           const data = await res.json();
           console.log(data);
           dispatch(signInSuccess(data));
-          //navigate('/');
+          navigate('/');
         } catch (error) {
           console.log('could not login with google', error);
         }
